f024f16b530fe3da0f22a0b3c9c70953c97f26c1
bodrulamin <bodrulaminiu@gmail.com>
2022-01-22 08:38:01 +0600

    m

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -20,34 +20,46 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
-      child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Login Form",
-              style: TextStyle(fontSize: 20.0),
-            ),
-            const SizedBox(height: 40),
-            TextFormField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Username"),
-            ),
-            const SizedBox(height: 10),
-            TextFormField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Password"),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(child: const Text("Login"), onPressed: () {}),
-              ],
-            ),
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Image(
+            image: AssetImage("images/lockimage.png"),
+            height: 100,
+            width: 100,
+          ),
+          const Text(
+            "Login Form",
+            style: TextStyle(fontSize: 20.0, fontFamily: 'Bebas'),
+          ),
+          const SizedBox(height: 40),
+          TextFormField(
+
+            decoration: const InputDecoration(
+                icon: Icon(Icons.account_circle),
+                border: OutlineInputBorder(),
+                labelText: "Username"),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: const InputDecoration(
+                icon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+                labelText: "Password"),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(child: const Text("Login"), onPressed: () {
+
+
+
+              }),
+            ],
+          ),
+        ],
       ),
     );
   }
